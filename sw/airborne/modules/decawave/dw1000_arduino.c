1a67836af1adc7d85c7d36a8754422c7eefe49a5
woeileong1104 <woeileong1104@gmail.com>
2018-02-27 13:03:05 +0800

    commit before switching computer

--- sw/airborne/modules/decawave/dw1000_arduino.c
+++ sw/airborne/modules/decawave/dw1000_arduino.c
@@ -40,61 +40,8 @@
 #include <string.h>
 #include "subsystems/datalink/telemetry.h" // added for telemetry of anchor range 20180202
 
-/** Number of anchors
- *
- * using standard trilateration algorithm, only 3 anchors are required/supported
- * at the moment.
- * More advanced multilateration algorithms might allow more anchors in the future
- */
-#ifndef DW1000_NB_ANCHORS
-#define DW1000_NB_ANCHORS 3
-#endif
-
-/** default offset, applied to final result not to individual distances */
-#ifndef DW1000_OFFSET
-#define DW1000_OFFSET { 0.f, 0.f, 0.f }
-#endif
-
-/** default scale factor, applied to final result not to individual distances */
-#ifndef DW1000_SCALE
-#define DW1000_SCALE { 1.f, 1.f, 1.f }
-#endif
-
-/** default initial heading correction between anchors frame and global frame */
-#ifndef DW1000_INITIAL_HEADING
-#define DW1000_INITIAL_HEADING 0.f
-#endif
-
-/** default timeout (in ms) */
-#ifndef DW1000_TIMEOUT
-#define DW1000_TIMEOUT 1000 // originally 500, changed to 600 on 20180205 to accomodate LPS Mini
-#endif
-
-/** frame sync byte */
-#define DW_STX 0xFE
-
-/** Parsing states */
-#define DW_WAIT_STX 0
-#define DW_GET_DATA 1
-#define DW_GET_CK 2
-#define DW_NB_DATA 6
-
-/** DW1000 positionning system structure */
-struct DW1000 {
-  uint8_t buf[DW_NB_DATA];    ///< incoming data buffer
-  uint8_t idx;                ///< buffer index
-  uint8_t ck;                 ///< checksum
-  uint8_t state;              ///< parser state
-  float initial_heading;      ///< initial heading correction
-  struct Anchor anchors[DW1000_NB_ANCHORS];   ///<anchors data
-  struct EnuCoor_f pos;       ///< local pos in anchors frame
-  struct EnuCoor_f raw_pos;   ///< unscaled local pos in anchors frame
-  struct GpsState gps_dw1000; ///< "fake" gps structure
-  struct LtpDef_i ltp_def;    ///< ltp reference
-  bool updated;               ///< new anchor data available
-};
-
-static struct DW1000 dw1000;
+//static struct DW1000 dw1000;
+struct DW1000 dw1000; // not "static" because need to be accessed elsewhere, changed on 20180208
 
 uint8_t dw_status; // added for debug 20180205
 
--- sw/airborne/modules/decawave/dw1000_arduino.h
+++ sw/airborne/modules/decawave/dw1000_arduino.h
@@ -19,12 +19,71 @@
  */
 /**
  * @file "modules/decawave/dw1000_arduino.h"
- * @author Gautier Hattenberger
+ * @author Gautier Hattenberger, modified by Woei-Leong Chan 
  * Driver to get ranging data from Decawave DW1000 modules connected to Arduino
  */
 
 #ifndef DW1000_ARDUINO_H
 #define DW1000_ARDUINO_H
+
+/** Number of anchors
+ *
+ * using standard trilateration algorithm, only 3 anchors are required/supported
+ * at the moment.
+ * More advanced multilateration algorithms might allow more anchors in the future
+ */
+#ifndef DW1000_NB_ANCHORS
+#define DW1000_NB_ANCHORS 3
+#endif
+
+/** default offset, applied to final result not to individual distances */
+#ifndef DW1000_OFFSET
+#define DW1000_OFFSET { 0.f, 0.f, 0.f }
+#endif
+
+/** default scale factor, applied to final result not to individual distances */
+#ifndef DW1000_SCALE
+#define DW1000_SCALE { 1.f, 1.f, 1.f }
+#endif
+
+/** default initial heading correction between anchors frame and global frame */
+#ifndef DW1000_INITIAL_HEADING
+#define DW1000_INITIAL_HEADING 0.f
+#endif
+
+/** default timeout (in ms) */
+#ifndef DW1000_TIMEOUT
+#define DW1000_TIMEOUT 1000 // originally 500, changed to 600 on 20180205 to accomodate LPS Mini
+#endif
+
+/** frame sync byte */
+#define DW_STX 0xFE
+
+/** Parsing states */
+#define DW_WAIT_STX 0
+#define DW_GET_DATA 1
+#define DW_GET_CK 2
+#define DW_NB_DATA 6
+
+#include "trilateration.h"
+#include "subsystems/gps.h"
+
+/** DW1000 positionning system structure, shifted here on 20180208 for value update through GCS */
+struct DW1000 {
+  uint8_t buf[DW_NB_DATA];    ///< incoming data buffer
+  uint8_t idx;                ///< buffer index
+  uint8_t ck;                 ///< checksum
+  uint8_t state;              ///< parser state
+  float initial_heading;      ///< initial heading correction
+  struct Anchor anchors[DW1000_NB_ANCHORS];   ///<anchors data
+  struct EnuCoor_f pos;       ///< local pos in anchors frame
+  struct EnuCoor_f raw_pos;   ///< unscaled local pos in anchors frame
+  struct GpsState gps_dw1000; ///< "fake" gps structure
+  struct LtpDef_i ltp_def;    ///< ltp reference
+  bool updated;               ///< new anchor data available
+};
+
+extern struct DW1000 dw1000; // added on 20180208 for value update through GCS
 
 extern void dw1000_arduino_init(void);
 extern void dw1000_arduino_periodic(void);
